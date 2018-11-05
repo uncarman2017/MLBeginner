933894db3eb08b2de09bc76112c1d18012eb30e0
Max Yu <31849893+uncarman2017@users.noreply.github.com>
2018-11-05 21:33:54 +0800

    kNN算法示例代码更新

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -6,7 +6,7 @@ Input:      inX: vector to compare to existing dataset (1xN)
             dataSet: size m data set of known vectors (NxM)
             labels: data set labels (1xM vector)
             k: number of neighbors to use for comparison (should be an odd number)
-            
+
 Output:     the most popular class label
 
 @author: pbharrin
@@ -15,10 +15,11 @@ Output:     the most popular class label
 '''
     k近值算法示例
 '''
+import matplotlib.pyplot as plt
+import operator
+
 from numpy import *
 from numpy.ma.core import *
-import operator
-from os import listdir
 
 '''
 使用k近邻算法改进约会网站的配对效果,算法步骤如下:
@@ -33,61 +34,31 @@ from os import listdir
 
 def dating_class_test():
     hoRatio = 0.50  # hold out 10%
-    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')  # 从指定文件中载入数据
+    # 从指定文件中载入数据,载入数据为每年获得的飞行常客里程数,玩视频游戏所耗时间百分比,每周消费的冰淇淋公升数
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
     normMat, ranges, minVals = autoNorm(datingDataMat)
-    m = normMat.shape[0]
-    numTestVecs = int(m * hoRatio)
+    # print(normMat[0:20])
+    # print(ranges)
+    # print(minVals)
+    # exit()
+
+    m = normMat.shape[0] # 取矩阵行数,rage函数返回包含行列数的元组对象
+    numTestVecs = int(m * hoRatio)  # 取测试行数
     errorCount = 0.0
     for i in range(numTestVecs):
+        # 取出矩阵每一行,非测试行,非测试行的分类标签
         classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)  # 求预测分类值
         print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
         if (classifierResult != datingLabels[i]): errorCount += 1.0  # 预测分类与实际类别不同，则标记为一个错误
     print("the total error rate is: %f, error count is %d" % (errorCount / float(numTestVecs), errorCount))
 
 
-'''
-使用k近邻算法实现手写识别系统
-(1) 收集数据：提供文本文件。
-(2) 准备数据：编写函数classify0()，将图像格式转换为分类器使用的list格式。
-(3) 分析数据：在Python命令提示符中检查数据，确保它符合要求。
-(4) 训练算法：此步骤不适用于k-近邻算法。
-(5) 测试算法：编写函数使用提供的部分数据集作为测试样本，测试样本与非测试样本的区别在于测试样本是已经完成分类的数据，如果预测分类与实际类别不同，则标记为一个错误。
-(6) 使用算法：本例没有完成此步骤，若你感兴趣可以构建完整的应用程序，从图像中提取数字，并完成数字识别，美国的邮件分拣系统就是一个实际运行的类似系统。
-'''
-
-
-def handwriting_class_test():
-    hwLabels = []
-    trainingFileList = listdir('trainingDigits')  # load the training set
-    m = len(trainingFileList)
-    trainingMat = zeros((m, 1024))
-    for i in range(m):
-        fileNameStr = trainingFileList[i]
-        fileStr = fileNameStr.split('.')[0]  # take off .txt
-        classNumStr = int(fileStr.split('_')[0])
-        hwLabels.append(classNumStr)
-        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
-    testFileList = listdir('testDigits')  # iterate through the test set
-    errorCount = 0.0
-    mTest = len(testFileList)
-    for i in range(mTest):
-        fileNameStr = testFileList[i]
-        fileStr = fileNameStr.split('.')[0]  # take off .txt
-        classNumStr = int(fileStr.split('_')[0])
-        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
-        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
-        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
-        if (classifierResult != classNumStr): errorCount += 1.0
-    print("\nthe total number of errors is: %d" % errorCount)
-    print("\nthe total error rate is: %f" % (errorCount / float(mTest)))
-
-
 # 分类器方法,求预测分类值
 def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    sqDiffMat = diffMat ** 2
-    sqDistances = sqDiffMat.sum(axis=1)
+    dataSetSize = dataSet.shape[0]  # 行数
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet  # 求出测试样本与非测试样本的差值
+    sqDiffMat = diffMat ** 2 # 求方
+    sqDistances = sqDiffMat.sum(axis=1)  # 行求和
     distances = sqDistances ** 0.5
     sortedDistIndicies = distances.argsort()
 
@@ -107,19 +78,18 @@ def createDataSet():
     return group, labels
 
 
-# 将文本文件转换为NumPy能够解析的矩阵数据格式
+# 将文本文件转换为NumPy矩阵
 # Input: 文本文件路径
-# Output: 训练样本矩阵和类标签向量
+# Output: 包含训练样本数据的NumPy矩阵和类标签向量
 def file2matrix(filename):
     fr = open(filename)
-    numberOfLines = len(fr.readlines())  # get the number of lines in the file
-
-    returnMat = zeros((numberOfLines, 3))  # 创建给定类型的矩阵并初始化0
-    classLabelVector = []  # 初始化分类标签向量
+    numberOfLines = len(fr.readlines())  # 得到文件行数
+    returnMat = zeros((numberOfLines, 3))  # 创建Numpy矩阵并初始化0
+    classLabelVector = []  # 初始化分类标签向量,存放文本行中最后一列分类标签
     fr = open(filename)
     index = 0
     for line in fr.readlines():
-        line = line.strip()
+        line = line.strip()  # 去除回车符
         listFromLine = line.split('\t')
         returnMat[index, :] = listFromLine[0:3]  # 给矩阵填值
         classLabelVector.append(int(listFromLine[-1]))  # 取出最后一个字段作为标签值存入向量对象
@@ -127,10 +97,14 @@ def file2matrix(filename):
     return returnMat, classLabelVector
 
 
+# 归一化特征值,即将飞行公里数值转化为[0,1]区间值
+# newValue = (oldValue-min)/(max-min)
+# dataset: NumPy矩阵
+# 返回值：归一化的numPy矩阵, 最大最小飞行公里数的差值行, 最小矩阵行
 def autoNorm(dataSet):
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
-    ranges = maxVals - minVals
+    ranges = maxVals - minVals  # 取最大最小飞行公里数的差值
     normDataSet = zeros(shape(dataSet))
     m = dataSet.shape[0]
     normDataSet = dataSet - tile(minVals, (m, 1))  # 矩阵每一行都与最小矩阵行做差值运算
@@ -138,14 +112,42 @@ def autoNorm(dataSet):
     return normDataSet, ranges, minVals
 
 
-def img2vector(filename):
-    returnVect = zeros((1, 1024))
-    fr = open(filename)
-    for i in range(32):
-        lineStr = fr.readline()
-        for j in range(32):
-            returnVect[0, 32 * i + j] = int(lineStr[j])
-    return returnVect
 
+
+# 读取NumPy矩阵格式的特征值，显示为散列图
+def test1():
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    print(datingDataMat[0:20])
+    print(datingLabels[0:20])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))  # 玩视频游戏所占百分比,每周消耗的冰淇淋公升数
+    # plt.show()
+    ax.scatter(datingDataMat[:, 0], datingDataMat[:, 1], 15.0 * array(datingLabels), 15.0 * array(datingLabels))  # 玩视频游戏所占百分比,每周消耗的冰淇淋公升数
+    plt.show()
+
+def test2():
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    print(normMat)
+    print(minVals)
+    print(ranges)
+    print(normMat.shape)
+
+
+# print("========================================================")
+# test1()
+# print("========================================================")
+# test2()
 
 dating_class_test()
+
+
+
+
+
+
+
+
+
+
---
+++ Ch02/kNN2.py
@@ -0,0 +1,73 @@
+
+from numpy import *
+from numpy.ma.core import *
+import operator
+from os import listdir
+
+'''
+使用k近邻算法实现手写识别系统
+(1) 收集数据：提供文本文件。
+(2) 准备数据：编写函数classify0()，将图像格式转换为分类器使用的list格式。
+(3) 分析数据：在Python命令提示符中检查数据，确保它符合要求。
+(4) 训练算法：此步骤不适用于k-近邻算法。
+(5) 测试算法：编写函数使用提供的部分数据集作为测试样本，测试样本与非测试样本的区别在于测试样本是已经完成分类的数据，如果预测分类与实际类别不同，则标记为一个错误。
+(6) 使用算法：本例没有完成此步骤，若你感兴趣可以构建完整的应用程序，从图像中提取数字，并完成数字识别，美国的邮件分拣系统就是一个实际运行的类似系统。
+'''
+
+
+def handwriting_class_test():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')  # load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')  # iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount / float(mTest)))
+
+
+
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+
+# 分类器方法,求预测分类值
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
+
+    classCount = {}
+
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+handwriting_class_test()
